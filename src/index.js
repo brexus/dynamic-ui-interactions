4a96ac103590188820184080bbd775c19ceae6c7
brexus <92671269+brexus@users.noreply.github.com>
2023-08-02 17:37:59 +0200

    add a newline at the end

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-// images must be named integers >1 with extension "jpg"
-// f.e. "4.jpg", "5.jpg"
+// images must be named integers >1 with extension "jpg" f.e. "4.jpg", "5.jpg"
 
 (() => {
-    const leftArrow = document.getElementById("left-arrow");
-    const rightArrow = document.getElementById("right-arrow");
-    const slider = document.getElementById("slider");
+    const leftArrow = document.getElementById('left-arrow');
+    const rightArrow = document.getElementById('right-arrow');
+    const slider = document.getElementById('slider');
 
     let currentImg = 1;
 
-    let minImg = 1;
-    let maxImg = 3; // change the value if you add more photos
+    const minImg = 1;
+    const maxImg = 3; // change the value if you add more photos
 
     const loadImg = () => {
         slider.style.backgroundImage = `url(./img/${currentImg}.jpg)`;
     };
 
     const updateCurrentImgDot = () => {
-        const dots = document.querySelectorAll(".dot");
+        const dots = document.querySelectorAll('.dot');
 
-        for (let i = 0; i < maxImg; i++) {
-            if (dots[i].classList.contains("active-dot")) {
-                dots[i].classList.remove("active-dot");
-                dots[i].classList.add("disable-dot");
+        for (let i = 0; i < maxImg; i += 1) {
+            if (dots[i].classList.contains('active-dot')) {
+                dots[i].classList.remove('active-dot');
+                dots[i].classList.add('disable-dot');
             }
         }
 
@@ -30,9 +29,9 @@
     };
 
     leftArrow.addEventListener('click', () => {
-        if(currentImg === minImg) {
+        if (currentImg === minImg) {
             currentImg = maxImg;
-        } else if(currentImg > minImg) {
+        } else if (currentImg > minImg) {
             currentImg -= 1;
         }
         loadImg();
@@ -40,23 +39,22 @@
     });
 
     rightArrow.addEventListener('click', () => {
-        if(currentImg === maxImg) {
+        if (currentImg === maxImg) {
             currentImg = minImg;
-        } else if(currentImg < maxImg) {
+        } else if (currentImg < maxImg) {
             currentImg += 1;
         }
         loadImg();
         updateCurrentImgDot();
     });
 
-    const dotsContainer = document.getElementById("dots");
-    for (let i = 1; i <= maxImg; i++) {
-        let dot = document.createElement("div");
-        dot.classList.add("dot");
-        dot.classList.add("disable-dot");
+    const dotsContainer = document.getElementById('dots');
+    for (let i = 1; i <= maxImg; i += 1) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        dot.classList.add('disable-dot');
         dotsContainer.appendChild(dot);
     }
 
     updateCurrentImgDot();
-
 })();
